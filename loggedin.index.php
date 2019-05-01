aa2ee71ab91e5b6f4aa2af1294b282886f2ffcab
Losel Matos <loselmatos@gmail.com>
2019-05-01 20:31:34 +0200

    Profile section created. Picture and Username added

--- loggedin.index.php
+++ loggedin.index.php
@@ -1,3 +1,11 @@
-<div align="center">
-	<h1><span class="logged-span">Hooray, you are logged in as <b style="color: red;"><?php echo $_SESSION['username'] ?></b></span></h1>
+<div id="profile-page-wrapper">
+
+	<div class="profile-column">
+		<img src="<?php echo $_SESSION['profilePic'] ?>" class="profile-column-photo">
+		<p class="profile-column-username"><?php echo $_SESSION['username'] ?></p>
+	</div>
+
+	<div class="profile-posts-div">
+	</div>
+
 </div>
