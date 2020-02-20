dcfdd45e444c46aebaad537533ee5e192459f2c8
Losel Matos <loselmatos@gmail.com>
2020-02-20 12:57:14 +0100

    Updated signup style and fixed tab index

--- signup.php
+++ signup.php
@@ -1,15 +1,3 @@
-<?php
-
-/*Connects to db
-//include 'includes/dbconnect.inc.php';
-//$conn = OpenCon();
-
-//gets the header
-require 'header.php';
-*/
-?>
-
-
 <main>
 	<div id="signup-content" class="popup-form" style="display: none;">
 		<div id="signup-wrapper">
@@ -21,18 +9,18 @@ require 'header.php';
 
 				<div class="row">
 					<div class="column">
-						<input type="text" id="signup-fname" name="firstname" placeholder="First Name" value="<?php echo $firstName; ?>" required>
-						<input type="text" id="signup-uname" name="username" placeholder="Username" value="<?php echo $username; ?>" required>
-						<input type="email" id="signup-email" name="email" placeholder="Email" value="<?php echo $email; ?>" required>
-						Birthday: <input  type="date" name="birthday" id="signup-birthday" <?php echo 'max="' . date('Y') . '-' . date('m') . '-' . date('d') . '"'; ?> value="<?php echo $birthday; ?>" required>
+						<input type="text" id="signup-fname" name="firstname" placeholder="First Name" value="<?php echo $firstName; ?>" tabindex="1" required>
+						<input type="text" id="signup-uname" name="username" placeholder="Username" value="<?php echo $username; ?>" tabindex="3" required>
+						<input type="email" id="signup-email" name="email" placeholder="Email" value="<?php echo $email; ?>" tabindex="5" required>
+						Birthday: <input  type="date" name="birthday" id="signup-birthday" <?php echo 'max="' . date('Y') . '-' . date('m') . '-' . date('d') . '"'; ?> value="<?php echo $birthday; ?>" tabindex="7" required>
 					</div>
 
 					<div class="column">
-						<input type="text" id="signup-lname" name="lastname" placeholder="Last Name" value="<?php echo $lastName; ?>" required>
-						<input type="password" id="signup-pwd" name="pwd" placeholder="Password" required>
-						<input type="tel" id="signup-tel" name="tel" placeholder="Phone Number" value="<?php echo $tel; ?>" required>
+						<input type="text" id="signup-lname" name="lastname" placeholder="Last Name" value="<?php echo $lastName; ?>" tabindex="2" required>
+						<input type="password" id="signup-pwd" name="pwd" placeholder="Password" tabindex="4" required>
+						<input type="tel" id="signup-tel" name="tel" placeholder="Phone Number" value="<?php echo $tel; ?>" tabindex="6" required>
 						
-						<select id="signup-gender">
+						<select id="signup-gender" tabindex="8">
 							<option value="male" <?php if($gender == 'male') echo 'checked'; ?> required> Male </option>
 							<option value="female" <?php if($gender == 'female') echo 'checked'; ?> required> Female </option>
 						</select>
