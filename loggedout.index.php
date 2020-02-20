d98cdb232627263e0715b6474df91518d246cc4c
Losel Matos <loselmatos@gmail.com>
2020-02-20 12:03:53 +0100

    Changed introduction text

--- loggedout.index.php
+++ loggedout.index.php
@@ -1,22 +1,18 @@
 <div class="logged-out-container" align="center">
-	<h1><span class="logged-span">Hey, this is a login system designed by <a href="http://loselmatos.com" target="_blank" style="color: red; text-decoration: underline;">Losel Matos</a></span></h1>
+	<h1><span class="logged-span">Hey, this is a social media website designed by <a href="http://loselmatos.com" target="_blank" style="color: red; text-decoration: underline;">Losel Matos</a></span></h1>
 	<h3><span class="logged-span">You can try and play around with it. Why not start with creating an account under "signup"?</span></h3>
 	<h4><span class="logged-span">Anyways, here are some pictures of cats</span></h4>
 
 	<div class="picture-collage">
 		<div class="row">
-
 			<div class="column">
 				<img class="cat-pics" src="https://i.pinimg.com/originals/90/99/6f/90996f5ed39bf749c0108c1078aa7634.jpg">
 				<img class="cat-pics" src="https://pixel-creation.com/wp-content/uploads/funny-cats-gangster-hd-desktop-wallpaper-instagram-photo-800x800.jpg">
 			</div>
-
 			<div class="column">
 				<img class="cat-pics" src="https://www.dhresource.com/0x0s/f2-albu-g4-M00-80-11-rBVaEVm2nfqAG9t6AAdN8FRr4ys168.jpg/engra-ado-pirata-gato-roupas-para-pet-cat.jpg">
 				<img class="cat-pics" src="https://pixel-creation.com/wp-content/uploads/funny-cat-desktop-wallpapers-wallpaper-hd-wallpapers-pinterest-1-800x800.jpg">
 			</div>
-
-
 		</div>
 	</div>
 </div>
